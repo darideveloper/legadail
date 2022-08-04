b68df3ebc3469fd67af75174f53128d5041977be
daridev <hernandezdarifrancisco@gmail.com>
2022-08-04 02:49:18 -0500

    use mysql in local

--- legadail/settings.py
+++ legadail/settings.py
@@ -93,9 +93,13 @@ WSGI_APPLICATION = "legadail.wsgi.application"
 MAX_CONN_AGE = 600
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'legadail',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
